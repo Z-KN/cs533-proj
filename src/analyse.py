def3c32d3507fa9f16897c2bc308cfe6218d021f
Boru Chen <eebrchen@gmail.com>
2023-03-18 14:15:36 -0500

    dump out json

--- src/analyse.py
+++ src/analyse.py
@@ -3,6 +3,7 @@ import onnxruntime as ort
 import onnx.numpy_helper
 import numpy as np
 import argparse
+import json
 from collections import OrderedDict
 
 # Create an argparse object and add arguments
@@ -47,11 +48,38 @@ ort_outs = ort_session.run(outputs, inputs)
 ort_outs = [single_out.shape for single_out in ort_outs]
 ort_outs = OrderedDict(zip(outputs, ort_outs))
 
+# extend directory
+for input in model.graph.input:
+    dim_list = [str(d.dim_value) for d in input.type.tensor_type.shape.dim]
+    if len(dim_list) == 0:
+        ort_outs[input.name] = '()'
+    elif len(dim_list) == 1:
+        ort_outs[input.name] = '(' + dim_list[0] + ',)'
+    else:
+        ort_outs[input.name] = '(' + ', '.join(dim_list) + ')'
+for ini in model.graph.initializer:
+    dim_list = [str(dim) for dim in ini.dims]
+    if len(dim_list) == 0:
+        ort_outs[ini.name] = '()'
+    elif len(dim_list) == 1:
+        ort_outs[ini.name] = '(' + dim_list[0] + ',)'
+    else:
+        ort_outs[ini.name] = '(' + ', '.join(dim_list) + ')'
+
 # Traverse the nodes in the model
+node_list = []
 for i, node in enumerate(model.graph.node):
     print(f'Node {i}: {node.op_type}')
+    input_info_list = []
+    output_info_list = []
     for input_name in node.input:
         print(f'  Input: {input_name} {ort_outs.get(input_name)}')
+        input_info_list.append({'name': f'{input_name}', 'shape': f'{ort_outs.get(input_name)}'})
     for output_name in node.output:
         print(f'  Output: {output_name} {ort_outs.get(output_name)}')
-# print(ort_outs)
+        output_info_list.append({'name': f'{output_name}', 'shape': f'{ort_outs.get(output_name)}'})
+    node_list.append({'optype': f'{node.op_type}', 'input':input_info_list, 'output': output_info_list})
+
+with open(args.model + '_node_info.json', 'w') as f:
+    # Write the JSON string to the file
+    json.dump(node_list, f, indent=4)
