5aa1390d3c2640a2fff0bedef4d821f54c7fa522
Boru Chen <eebrchen@gmail.com>
2023-03-17 23:46:12 -0500

    argparse to choose resnet or transformer

--- src/analyse.py
+++ src/analyse.py
@@ -2,22 +2,31 @@ import onnx
 import onnxruntime as ort
 import onnx.numpy_helper
 import numpy as np
+import argparse
 from collections import OrderedDict
 
-# load ONNX model
-# model_path = 'resnet18-v2-7.onnx'
-model_path = 'transformer.onnx'
-# model=onnx.load(model_path)
-# input_info = model.graph.input
-# output_info = model.graph.output
+# Create an argparse object and add arguments
+parser = argparse.ArgumentParser(description='Analyzing transformer and resnet models.')
+parser.add_argument('--model', type=str, required=True, help='Model to analyze (transformer or resnet)')
 
-# print("Input shapes:")
-# for input in input_info:
-#     print("  ", input.name, ":", [d.dim_value for d in input.type.tensor_type.shape.dim])
+# Parse the arguments
+args = parser.parse_args()
 
-# print("Output shapes:")
-# for output in output_info:
-#     print("  ", output.name, ":", [d.dim_value for d in output.type.tensor_type.shape.dim])
+# Check which model was selected
+if args.model == 'transformer':
+    print('Analyzing transformer model...')
+    # Call function to analyze transformer model
+    model_path = 'transformer.onnx'
+    in_img = [np.random.randn(10, 1, 128).astype(np.float32), \
+        np.random.randn(10, 1, 128).astype(np.float32)]
+elif args.model == 'resnet':
+    print('Analyzing resnet model...')
+    # Call function to analyze resnet model
+    model_path = 'resnet18-v2-7.onnx'
+    in_img = [np.random.randn(1,3,224,224).astype(np.float32)]
+else:
+    print('Error: Model not recognized. Please choose transformer or resnet.')
+    exit(1)
 
 ort_session = ort.InferenceSession(model_path)
 org_outputs = [x.name for x in ort_session.get_outputs()]
@@ -31,12 +40,10 @@ for node in model.graph.node:
 # execute onnx
 ort_session = ort.InferenceSession(model.SerializeToString())
 outputs = [x.name for x in ort_session.get_outputs()]
-# in_img = np.random.randn(1,3,224,224).astype(np.float32)
-x = np.random.randn(10, 1, 128).astype(np.float32)
-y = np.random.randn(10, 1, 128).astype(np.float32)
-ort_outs = ort_session.run(outputs, {ort_session.get_inputs()[0].name: x, \
-                                     ort_session.get_inputs()[1].name: y} )
-# ort_outs = ort_session.run(outputs, {ort_session.get_inputs()[0].name: in_img} )
+inputs = {}
+for input_name, input_value in zip(ort_session.get_inputs(), in_img):
+    inputs[input_name.name] = input_value
+ort_outs = ort_session.run(outputs, inputs)
 ort_outs = [single_out.shape for single_out in ort_outs]
 ort_outs = OrderedDict(zip(outputs, ort_outs))
 print(ort_outs)
